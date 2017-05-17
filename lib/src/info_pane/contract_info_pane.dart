c5e2b3773adf44feb71e94882bf19028c70949b0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-17 16:15:39 +0300

    CMAS-344 Мультивалютность договоров на frontend'e

--- lib/src/info_pane/contract_info_pane.dart
+++ lib/src/info_pane/contract_info_pane.dart
@@ -2,11 +2,14 @@ import 'package:angular2/core.dart';
 
 import 'package:contracts/contract_general_model.dart';
 import 'package:contracts/contracts_service.dart';
+import 'package:angular_utils/cm_format_money_pipe.dart';
 
 @Component(
     selector: 'contract-info-pane',
     providers: const [ContractsService],
-    templateUrl: 'contract_info_pane.html')
+    templateUrl: 'contract_info_pane.html',
+    pipes: const [CmFormatMoneyPipe]
+)
 class ContractInfoPaneComponent implements OnInit {
   final ContractsService _service;
 
